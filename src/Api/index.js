0648cb3bf5ddd81ef759823df8aa7c59576c851f
ThiagoSouza <thiagosouzasi32@gmail.com>
2021-12-28 20:21:44 -0300

    Finalizado requisição

---
+++ src/Api/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_KEY = 'b06bd1cccece485da5b223227212812';
+
+const api = axios.create({
+    baseURL:`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=crato`,
+  });
+
+export default api;
+
+
+
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import Api from '../../Api';
 
 export default function Home(){
+
+    useEffect(()=>{
+        getCurrentClimate();
+    },[]);
+
+    const getCurrentClimate = ()=>{
+        Api.get()
+        .then((reponse)=>{
+            console.log(JSON.stringify(reponse.data));
+        })
+        .catch((error)=>{
+            console.log('erro ao buscar dados' + error)
+        })
+    }
+
     return(
         <>
             <Text>Estou funcionando</Text>
