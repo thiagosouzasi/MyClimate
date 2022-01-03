9fa3765f2453b6a90aa834d9a02d9757c712f975
ThiagoSouza <thiagosouzasi32@gmail.com>
2022-01-03 15:06:42 -0300

    Ajuste para background

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -9,8 +9,8 @@ import RainDay from '../../Assets/rainday.png';
 import CloudyDay from '../../Assets/cloudyday.png';
 
 import Nigth from '../../Assets/nigth.png';
-import Rainnigth from '../../Assets/rainnigth.png';
-import Cloudynigth from '../../Assets/cloudynigth.png';
+import RainNigth from '../../Assets/rainnigth.png';
+import CloudyNigth from '../../Assets/cloudynigth.png';
 
 
 import styles from './styles';
@@ -24,6 +24,11 @@ export default function Home(){
     const [location, setLocation] = useState('');
     const [currentCLimate,setCurrentClimate] =useState();
     const [nextDays, setNextDays] =  useState([]);
+
+    const CLOUDY_CONDITIONS = [1003,1006,1009,1030,1135];
+    const RAIN_CONDITIONS   = [1063,1087,1150,1153,1168,1171,1180,1183,1186,1189,1192,1195,1198,1201,1240,1243,1246,1273,1276];
+    const SNOW_CONDITIONS   = [1066,1069,1072,1114,1117,1147,1204,1207,1210,1213,1216,1919,1219,1225,1237,1249,1252,1255,1258,1261,1264,1279,1282];
+    const SUNNY_CONDITIONS  = [1000];
 
     useEffect(()=>{
         getCurrentLocation();
@@ -83,20 +88,7 @@ export default function Home(){
         }
     }
 
-    const setBackgroundImage = (conditionCode,dayNigth)=>{
-        try {
-            if(dayNigth === 0){
-                switch (conditionCode){
-                    case 1063:
-                }
-                
-            }else{
-
-            }
-        } catch (error) {
-            console.log('Erro ao selecionar fundo' + error);
-        }
-    }
+    
     
     const getCurrentClimate = ()=>{
         if(latitude){
@@ -119,6 +111,26 @@ export default function Home(){
             return <ClimateDay day={item.item.date} max={item.item.day.maxtemp_c} min={item.item.day.mintemp_c}/>
         } catch (error) {
             console.log('Erro ao renderizar next days' + error);
+        }
+    }
+
+    const setBackGroundImage = (climateCode)=>{
+        try {
+            if(SUNNY_CONDITIONS.includes(climateCode)){
+                
+                
+            }else if(RAIN_CONDITIONS.includes(climateCode)){                
+                currentCLimate.is_day === 1 ? setBackGround(RainDay) : setBackGround(RainNigth);
+
+            }else if(CLOUDY_CONDITIONS.includes(climateCode)){
+                currentCLimate.is_day === 1 ? setBackGround(CloudyDay) : setBackGround(CloudyNigth);
+
+            }else if(SNOW_CONDITIONS.includes(climateCode)){
+                
+            }
+
+        } catch (error) {
+            console.log(`Erro ao setar backgroudn ${error}`);
         }
     }
 
